63e9851d790daba3faed67e6aa4947163578c338
Muhammad Basit <139772050+muhbasit@users.noreply.github.com>
2023-07-17 20:17:05 +0400

    Create 3.Syntax.py

---
+++ Python/3.Syntax.py
@@ -0,0 +1,8 @@
+# syntax of Python file is very simple
+# i.e. We want to display some text on screen
+
+print("Hello World") 
+# it'll show Hello World on screen 
+x = 5
+print(x)
+# it'll show 5 on screen 
