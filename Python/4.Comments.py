c5e2c09e6d85d86ff76de4982433a1ad9ed2d987
Muhammad Basit <139772050+muhbasit@users.noreply.github.com>
2023-07-17 20:23:25 +0400

    Create 4.Comments.py

---
+++ Python/4.Comments.py
@@ -0,0 +1,25 @@
+#Comments
+#Non executable programming code. 
+#Comments can be used to explain Python code.
+#Comments can be used to make the code more readable.
+#Comments can be used to prevent execution when testing code.
+#Comments starts with a #, and Python will ignore them. 
+
+#This is a comment
+print("Hello, World!")
+
+# I'll show only Hello, World! Nothing else
+# orher that that is comments 
+
+# Multi-line comments
+# denoted by """ {} """
+#Since Python will ignore string literals that are not assigned 
+#to a variable, you can add a multiline string (triple quotes) in 
+#your code, and place your comment inside it. 
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
