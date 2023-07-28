3925547b7f2499de3820ed901582a4e35102b7f0
Muhammad Basit <139772050+muhbasit@users.noreply.github.com>
2023-07-28 09:20:52 +0400

    Create DataType.py

---
+++ Python/Day 2/DataType1.py
@@ -0,0 +1,98 @@
+x = 5
+print(x)
+print(type(x))
+
+y  = "Hello World"
+#display y:
+print(y)
+#display the data type of y:
+print(type(y)) 
+
+z= 20
+#display z:
+print(z)
+#display the data type of z:
+print(type(z))
+
+a = 20.5
+#display a:
+print(a)
+#display ta = 20.5
+#display a:
+print(a)
+#display the data type of :
+print(type(a)) 
+he data type of :
+print(type(a)) 
+
+b = 1j
+#display b:
+print(b)
+#display the data type of b:
+print(type(b)) 
+
+list = ["apple", "banana", "cherry"]
+#display list:
+print(list)
+#display the data type of list:
+print(type(list)) 
+
+tup = ("apple", "banana", "cherry")
+#display tup:
+print(tup)
+#display the data type of tup:
+print(type(tup)) 
+
+dict = {"name" : "John", "age" : 36}
+#display dict:
+print(dict)
+#display the data type of dict:
+print(type(dict)) 
+
+r = range(6)
+#display r:
+print(r)
+#display the data type of r:
+print(type(r)) 
+
+seT = {"apple", "banana", "cherry"}
+#display seT:
+print(seT)
+#display the data type of seT:
+print(type(seT)) 
+
+rSet = frozenset({"apple", "banana", "cherry"})
+#display fSet:
+print(fSet)
+#display the data type of fSet:
+print(type(fSet)) 
+
+Bool = True
+#display Bool:
+print(Bool)
+#display the data type of Bool:
+print(type(Bool)) 
+
+byte = b"Hello"
+#display byte:
+print(byte)
+#display the data type of byte:
+print(type(byte)) 
+
+bArray = bytearray(5)
+#display bArray:
+print(bArray)
+#display the data type of bArray:
+print(type(bArray)) 
+
+mView = memoryview(bytes(5))
+#display mView:
+print(mView)
+#display the data type of mView:
+print(type(mView)) 
+
+none = None
+#display none:
+print(none)
+#display the data type of none:
+print(type(none)) 
