b2b0ce788cac73bd65d44532a83d4396f0ffbbc4
Muhammad Basit <139772050+muhbasit@users.noreply.github.com>
2023-07-28 12:04:46 +0400

    Create StringMethods.py

---
+++ Python/Day 3/StringMethods.py
@@ -0,0 +1,13 @@
+txt = "hello, and welcome to my world."
+
+x = txt.capitalize()
+print (x)
+# output will be
+# Hello, and welcome to my world. 
+
+y = txt.casefold()
+print(y)
+# output will be 
+# hello, and welcome to my world. 
+
+
