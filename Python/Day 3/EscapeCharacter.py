6fe1f7db75c753cfc1cdb111f519138b36a53994
Muhammad Basit <139772050+muhbasit@users.noreply.github.com>
2023-07-29 10:47:10 +0400

    Create EscapeCharacter.py

---
+++ Python/Day 3/EscapeCharacter.py
@@ -0,0 +1,39 @@
+# Double Quote
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt) 
+
+# Single Quote
+txt = 'It\'s alright.'
+print(txt) 
+
+# Back Slash
+txt = "We are the so-called \\Vikings from the north."
+print(txt) 
+
+# New Line 
+txt = "We are the so-called \nVikings from the north."
+print(txt) 
+
+# Carriage Return 
+txt = "We are the so-called \rVikings from the north."
+print(txt) 
+
+# Tab
+txt = "We are the so-called \tVikings from the north."
+print(txt) 
+
+# Back Space
+txt = "We are the so-called  \bVikings from the north."
+print(txt) 
+
+# Form Fees
+txt = "We are the so-called \fVikings from the north."
+print(txt) 
+
+# Octal Value
+txt = "We are the so-called \oooVikings from the north."
+print(txt) 
+
+# Hex Values 
+txt = "We are the so-called \xhhVikings from the north."
+print(txt) 
